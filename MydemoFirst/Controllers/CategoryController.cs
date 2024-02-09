9d55eeb3b1deb4952bd1e4fb064cf26cba7eaa64
thangkhovipro <thangkhovipro@gmail.com>
2024-02-09 20:13:29 +0700

    lythevinh

--- MydemoFirst/Controllers/CategoryController.cs
+++ MydemoFirst/Controllers/CategoryController.cs
@@ -100,7 +100,7 @@ namespace MydemoFirst.Controllers
             var result = new GenericResponse<CategoryResponse>
             {
 
-                Msg = "update thanh congggggg1232132131233124asdsa",
+                Msg = "update thanh lythevinh",
 
                 Values = newCategory
 

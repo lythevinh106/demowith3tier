f6b2f799d7522b2b3fcdacc49ce4621ec1a6927f
lythevinh106 <105789682+lythevinh106@users.noreply.github.com>
2024-02-09 20:11:59 +0700

    Update CategoryController.cs

--- MydemoFirst/Controllers/CategoryController.cs
+++ MydemoFirst/Controllers/CategoryController.cs
@@ -100,7 +100,7 @@ namespace MydemoFirst.Controllers
             var result = new GenericResponse<CategoryResponse>
             {
 
-                Msg = "update thanh congggggg1232132131233124asdsadsadasdasdaasda",
+                Msg = "update thanh congggggg1232132131233124asdsa",
 
                 Values = newCategory
 
